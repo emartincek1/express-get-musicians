95b911a572b49bac541be8c6cf0eda6726c8b2c0
emartincek1 <evan.martincek@gmail.com>
2024-02-19 09:31:30 -0600

    add get route to musicians and tests

--- index.test.js
+++ index.test.js
@@ -1,22 +1,26 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
 const seedMusician = require("./seedData");
 
+describe("./musicians endpoint", () => {
+  // Write your tests here
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
-})
+  test("Testing musicians endpoint", async () => {
+    const response = await request(app).get("/musicians");
+    expect(response.statusCode).toBe(200);
+    const responseData = JSON.parse(response.text);
+    expect(responseData[0]).toEqual(
+      expect.objectContaining({
+        name: "Mick Jagger",
+        instrument: "Voice",
+      })
+    );
+  });
+});
