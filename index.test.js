a63e44236692ee4fdfe78434291835b8472311c4
emartincek1 <evan.martincek@gmail.com>
2024-02-21 11:42:37 -0600

    add musician crud routes, validation, and tests

--- index.test.js
+++ index.test.js
@@ -12,7 +12,7 @@ const seedMusician = require("./seedData");
 describe("./musicians endpoint", () => {
   // Write your tests here
 
-  test("Testing musicians endpoint", async () => {
+  test("Testing get musicians endpoint", async () => {
     const response = await request(app).get("/musicians");
     expect(response.statusCode).toBe(200);
     const responseData = JSON.parse(response.text);
@@ -22,5 +22,65 @@ describe("./musicians endpoint", () => {
         instrument: "Voice",
       })
     );
+  });
+
+  test("Testing get musicians/:id endpoint", async () => {
+    const response = await request(app).get("/musicians/1");
+    expect(response.statusCode).toBe(200);
+    const responseData = JSON.parse(response.text);
+    expect(responseData).toEqual(
+      expect.objectContaining({
+        name: "Mick Jagger",
+        instrument: "Voice",
+      })
+    );
+  });
+
+  test("post musicians returns correct response", async () => {
+    const response = await request(app)
+      .post("/musicians")
+      .send({
+        musician: {
+          name: "50 Cent",
+          instrument: "Voice",
+        },
+      });
+    expect(response.statusCode).toBe(200);
+    expect(response.body[3]).toEqual(
+      expect.objectContaining({
+        name: "50 Cent",
+        instrument: "Voice",
+      })
+    );
+    const musicians = await Musician.findAll();
+    expect(response.body.length).toBe(musicians.length);
+  });
+
+  test("put musicians returns correct response", async () => {
+    const response = await request(app)
+      .put("/musicians/4")
+      .send({
+        musician: {
+          name: "Lil Wayne",
+          instrument: "Voice",
+        },
+      });
+    expect(response.statusCode).toBe(200);
+    expect(response.body[3]).toEqual(
+      expect.objectContaining({
+        name: "Lil Wayne",
+        instrument: "Voice",
+      })
+    );
+    const musicians = await Musician.findAll();
+    expect(response.body.length).toBe(musicians.length);
+  });
+
+  test("delete musicians returns correct response", async () => {
+    const response = await request(app).delete("/musicians/4");
+    expect(response.statusCode).toBe(200);
+    expect(response.body[3]).toEqual(undefined);
+    const musicians = await Musician.findAll();
+    expect(response.body.length).toBe(musicians.length);
   });
 });
--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const { Musician } = require("../models/index");
 const { db } = require("../db/connection");
+const { check, validationResult } = require("express-validator");
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/musicians", async (req, res) => {
   const musicians = await Musician.findAll();
@@ -18,6 +20,68 @@ app.get("/musicians/:id", async (req, res, next) => {
     const musician = await Musician.findByPk(id);
     if (musician === null) return res.send(`No musician at ${id}.`);
     res.json(musician);
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.post(
+  "/musicians",
+  [
+    check("musician.name").not().isEmpty().trim(),
+    check("musician.instrument").not().isEmpty().trim(),
+  ],
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.json({ error: errors.array() });
+      } else {
+        const { musician } = req.body;
+        await Musician.create(musician);
+        const musicians = await Musician.findAll();
+        res.json(musicians);
+      }
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+app.put(
+  "/musicians/:id",
+  [
+    check("musician.name").not().isEmpty().trim(),
+    check("musician.instrument").not().isEmpty().trim(),
+  ],
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.json({ error: errors.array() });
+      } else {
+        const { musician } = req.body;
+        const { id } = req.params;
+        const oldMusician = await Musician.findByPk(id);
+        if (oldMusician === null) return res.send(`No musician at ${id}.`);
+        await oldMusician.update(musician);
+        const musicians = await Musician.findAll();
+        res.json(musicians);
+      }
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+app.delete("/musicians/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const oldMusician = await Musician.findByPk(id);
+    if (oldMusician === null) return res.send(`No musician at ${id}.`);
+    await oldMusician.destroy();
+    const musicians = await Musician.findAll();
+    res.json(musicians);
   } catch (e) {
     next(e);
   }
