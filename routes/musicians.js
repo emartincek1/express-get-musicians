ef64e65ea0a946b5e4b60949202cae1965bed0e9
emartincek1 <evan.martincek@gmail.com>
2024-02-22 08:19:40 -0600

    change structure to musician router

---
+++ routes/musicians.js
@@ -0,0 +1,84 @@
+const { Router } = require("express");
+const musicianRouter = Router();
+const { Musician } = require("../models/index");
+const { check, validationResult } = require("express-validator");
+
+musicianRouter.get("/", async (req, res) => {
+  const musicians = await Musician.findAll();
+  res.json(musicians);
+});
+
+musicianRouter.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const musician = await Musician.findByPk(id);
+    if (musician === null) return res.send(`No musician at ${id}.`);
+    res.json(musician);
+  } catch (e) {
+    next(e);
+  }
+});
+
+musicianRouter.post(
+  "/",
+  [
+    check("musician.name").not().isEmpty().trim(),
+    check("musician.instrument").not().isEmpty().trim(),
+  ],
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.json({ error: errors.array() });
+      } else {
+        const { musician } = req.body;
+        await Musician.create(musician);
+        const musicians = await Musician.findAll();
+        res.json(musicians);
+      }
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+musicianRouter.put(
+  "/:id",
+  [
+    check("musician.name").not().isEmpty().trim(),
+    check("musician.instrument").not().isEmpty().trim(),
+  ],
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.json({ error: errors.array() });
+      } else {
+        const { musician } = req.body;
+        const { id } = req.params;
+        const oldMusician = await Musician.findByPk(id);
+        if (oldMusician === null) return res.send(`No musician at ${id}.`);
+        await oldMusician.update(musician);
+        const musicians = await Musician.findAll();
+        res.json(musicians);
+      }
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+musicianRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const oldMusician = await Musician.findByPk(id);
+    if (oldMusician === null) return res.send(`No musician at ${id}.`);
+    await oldMusician.destroy();
+    const musicians = await Musician.findAll();
+    res.json(musicians);
+  } catch (e) {
+    next(e);
+  }
+});
+
+module.exports = musicianRouter;
